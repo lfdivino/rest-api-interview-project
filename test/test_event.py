deb9af7cb2293fddf326962baa1ec64a14784f72
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-29 10:21:58 -0300

    refactoring of create event test cases

--- test/test_event.py
+++ test/test_event.py
@@ -2,14 +2,15 @@
 
 import unittest
 from app.core import core_app
-from .constants import *
+from test.constants import *
+from app.db_event_commands import EventCommandsDB
 
 
 class TestEventUsingRoutes(unittest.TestCase):
     def setUp(self):
         self.api = core_app().test_client()
 
-    def test_create_new_event(self):
+    def create_new_event(self):
         result = self.api.post('/api/v1/event', json=JSON_NEW_EVENT)
         self.assertEqual(
             result.json,
@@ -17,13 +18,31 @@ class TestEventUsingRoutes(unittest.TestCase):
             "Event can't be created!"
         )
 
-    def test_create_new_event_without_send_json(self):
+    def create_new_event_without_send_json(self):
         result = self.api.post('/api/v1/event')
         self.assertEqual(
             result.json,
-            'Invalid request json',
+            "Invalid request json",
             "Error message not displayed!"
         )
+
+    def create_event_that_exist_in_the_collection(self):
+        result = self.api.post('/api/v1/event', json=JSON_NEW_EVENT)
+        self.assertEqual(
+            result.json,
+            'The event id 8663424224 already exists!',
+            'The event with the same id was created!'
+        )
+
+    def delete_event_by_id(self):
+        event_db = EventCommandsDB()
+        event_db.delete_event(JSON_NEW_EVENT['id'])
+
+    def test_create_event(self):
+        self.create_new_event()
+        self.create_new_event_without_send_json()
+        self.create_event_that_exist_in_the_collection()
+        self.delete_event_by_id()
 
 
 if __name__ == '__main__':
