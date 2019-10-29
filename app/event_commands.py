b3417b6dcbc7e42e25f03ec2e3d7b9cb2521c6fd
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-29 10:20:53 -0300

    refactoring create_event() to save in the database

--- app/event_commands.py
+++ app/event_commands.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from .db_event_commands import EventCommandsDB
+
 
 class Event(object):
     def __init__(self, json):
         self.json_event = json
 
     def create_event(self):
+        db_event = EventCommandsDB()
+        for event in db_event.select_event_by_id(self.json_event['id']):
+            return 'The event id {} already exists!'.format(event['id']), 400
+
+        result = db_event.create_event(self.json_event)
+
         return self.json_event['id'], 200
