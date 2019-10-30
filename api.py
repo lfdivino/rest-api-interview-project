23916b1ab4c57048cb384d678fb1f97173bf8875
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-30 16:31:09 -0300

    refactoring the api serving run by werkzeug

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from werkzeug.serving import run_simple
 from app import core
 
 
 if __name__ == '__main__':
     app = core.core_app()
-    app.run()
+    run_simple('0.0.0.0', 5000, app, use_reloader=True)
