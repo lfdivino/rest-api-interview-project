9400b2bad89b65e7428d9bf3819c92610fd0ef57
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-30 17:10:54 -0300

    adding new routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from .event import NewEvent
+from app.event import Event
+from app.odds import Odds
+from app.match import MatchByID, MatchByArgs
 
 
 def add_routes(api):
-    api.add_resource(NewEvent, '/api/v1/event')
+    api.add_resource(Event, '/api/v1/event')
+    api.add_resource(MatchByID, '/api/v1/match/<event_id>')
+    api.add_resource(MatchByArgs, '/api/v1/match/')
+    api.add_resource(Odds, '/api/v1/odds')
