853ae51c19cb05ad2d6b11cc055b67856114af6f
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-30 17:11:03 -0300

    create settings files for db parameters

--- settings.py
+++ settings.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
-import os
 
-os.environ
+DB_MONGO_USER = 'api_supporter'
+DB_MONGO_PASS = 'sampa1621'
 
 # Database Settings
+# Connection URI ideal
+# MONGO_URI = "mongodb+srv://{}:{}@888-spectate-xmzvt.mongodb.net/" \
+#             "test?retryWrites=true&w=majority".format(
+#                 os.environ['DB_MONGO_USER'],
+#                 os.environ['DB_MONGO_PASS']
+
+# Connection URI for this project
 MONGO_URI = "mongodb+srv://{}:{}@888-spectate-xmzvt.mongodb.net/" \
             "test?retryWrites=true&w=majority".format(
-                os.environ['DB_MONGO_USER'],
-                os.environ['DB_MONGO_PASS']
+                DB_MONGO_USER,
+                DB_MONGO_PASS
             )
+
 MONGO_DATABASE = '888-spectate'
+
+DB_FIELDS_RETURN = {
+    "_id": 0,
+    "id": 1,
+    "url": 1,
+    "name": 1,
+    "startTime": 1
+}
