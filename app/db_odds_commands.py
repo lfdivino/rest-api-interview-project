6803a4cefc7b062aa84577ee774a5d2dfa00ebd6
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-30 16:39:32 -0300

    adding database commands for odds update

---
+++ app/db_odds_commands.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from app.db_event_commands import EventCommandsDB
+
+
+class OddsCommandDB(EventCommandsDB):
+    def update_odds(self, event_index_id, modified_event_odds):
+        return self.db.db_collection.update_one(
+            {'id': event_index_id},
+            {'$set': modified_event_odds}
+        )
