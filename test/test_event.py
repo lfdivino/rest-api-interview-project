099677bd7702fefedd346a6dbbc7195a235e6e09
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-30 16:41:01 -0300

    adding update odds test cases

--- test/test_event.py
+++ test/test_event.py
@@ -44,6 +44,36 @@ class TestEventUsingRoutes(unittest.TestCase):
         self.create_event_that_exist_in_the_collection()
         self.delete_event_by_id()
 
+    def update_odds_non_existed_event(self):
+        result = self.api.post('/api/v1/odds', json=JSON_UPDATE_ODDS)
+        self.assertEqual(
+            result.json,
+            "The event id 994839351740 don't exists!",
+            "Update odds of a different event!"
+        )
+
+    def update_odds_without_send_json(self):
+        result = self.api.post('/api/v1/odds')
+        self.assertEqual(
+            result.json,
+            "Invalid request json",
+            "Update odds of a non specified event!"
+        )
+
+    def update_odds_event(self):
+        result_create = self.api.post('/api/v1/event', json=JSON_NEW_EVENT)
+        result_update = self.api.post('/api/v1/odds', json=JSON_UPDATE_ODDS)
+        self.assertTrue(
+            result_update.json,
+            'It was not possible to update this event odds!'
+        )
+
+    def test_update_odds(self):
+        self.update_odds_non_existed_event()
+        self.update_odds_without_send_json()
+        self.update_odds_event()
+        self.delete_event_by_id()
+
 
 if __name__ == '__main__':
     unittest.main()
