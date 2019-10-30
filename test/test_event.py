19c3af106191da1fa75415572c7d51f96901521a
Luiz Felipe do Divino <lf.divino@gmail.com>
2019-10-30 17:10:39 -0300

    adding test cases for matches searchs

--- test/test_event.py
+++ test/test_event.py
@@ -37,6 +37,7 @@ class TestEventUsingRoutes(unittest.TestCase):
     def delete_event_by_id(self):
         event_db = EventCommandsDB()
         event_db.delete_event(JSON_NEW_EVENT['event']['id'])
+        event_db.delete_event(JSON_NEW_EVENT_02['event']['id'])
 
     def test_create_event(self):
         self.create_new_event()
@@ -72,6 +73,71 @@ class TestEventUsingRoutes(unittest.TestCase):
         self.update_odds_non_existed_event()
         self.update_odds_without_send_json()
         self.update_odds_event()
+        self.delete_event_by_id()
+
+    def search_match_by_id(self):
+        result_create = self.api.post('/api/v1/event', json=JSON_NEW_EVENT)
+        result_search = self.api.get('/api/v1/match/994839351740')
+        self.assertEqual(
+            result_search.json,
+            JSON_EVENT_BY_ID,
+            "The result of the search by id is different "
+            "from the event in the database"
+        )
+
+        result_search = self.api.get('/api/v1/match/231231312')
+        self.assertEqual(
+            result_search.json,
+            "Match not found!",
+            "The result of the search by non existing id find an event"
+        )
+
+    def search_match_by_name(self):
+        result_search = self.api.get(
+            '/api/v1/match/?name=Real Madrid vs Barcelona')
+        self.assertEqual(
+            result_search.json,
+            JSON_EVENT_BY_NAME,
+            "The result of the search by name is different "
+            "from the event in the database"
+        )
+
+    def search_match_by_sport_and_ordering(self):
+        result_search = self.api.get(
+            '/api/v1/match/?sport=Football&ordering=startTime')
+        self.assertEqual(
+            result_search.json,
+            JSON_EVENT_BY_SPORT,
+            "The result of the search by sport is different "
+            "from the event in the database"
+        )
+
+    def search_match_by_sport(self):
+        result_search = self.api.get(
+            '/api/v1/match/?sport=Football')
+        self.assertEqual(
+            result_search.json,
+            JSON_EVENT_BY_SPORT,
+            "The result of the search by sport is different "
+            "from the event in the database"
+        )
+
+    def search_matchs(self):
+        result_create = self.api.post('/api/v1/event', json=JSON_NEW_EVENT_02)
+        result_search = self.api.get('/api/v1/match/')
+        self.assertEqual(
+            len(result_search.json),
+            2,
+            "The result of the search of all matches is different "
+            "from the events in the database"
+        )
+
+    def test_match_search(self):
+        self.search_match_by_id()
+        self.search_match_by_name()
+        self.search_match_by_sport_and_ordering()
+        self.search_match_by_sport()
+        self.search_matchs()
         self.delete_event_by_id()
 
 
